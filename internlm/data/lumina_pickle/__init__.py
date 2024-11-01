572bdc8ecbf17bc6c2dbad1ffe18edec7ca30bd9
hujiakai <zxc200855909@qq.com>
2024-11-01 17:46:18 +0800

    Revert "Feat/heterogeneous x pu training (#355)"
    This reverts commit d6bfaca03b84ae6bad32da263b703f595a415473.

--- internlm/data/lumina_pickle/__init__.py
+++ internlm/data/lumina_pickle/__init__.py
@@ -1,8 +1,8 @@
-from .sampler import LuminaPickleBatchSampler
+from .sampler import LuminaPickleSampler
 from .dataset import LuminaPickleDataset
 
 __all__ = [
-    "LuminaPickleBatchSampler",
+    "LuminaPickleSampler",
     "LuminaPickleDataset"
 ]
 
